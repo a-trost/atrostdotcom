acfacb2e51bd516e4886105ac7027498178ab1fc
Alexander Trost <AlexRTrost@gmail.com>
2018-07-11 12:12:07 -0400

    content: Change body copy for About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ export default class About extends Component {
 
 				<p>I worked in a data-intense school and realized I wasn’t nearly as good at parsing the hundreds of data points as a computer would be. So my first summer vacation, I taught myself Python so I could make a Django webapp. My little school of 300 kids and teachers use it to make learning just a bit easier.</p>
 
-				<p>I tossed a lot of problems at my web app and wanted to take my learning further, so I got a Google Developer Challenge Scholarship to help me learn front end languages like JavaScript and frameworks like React. I've graduated with my <a href="" >Udacity Nanodegree</a> and </p>
+				<p>I tossed a lot of problems at my web app and wanted to take my learning further, so I got a Google Developer Challenge Scholarship to help me learn front end languages like JavaScript and frameworks like React. I've graduated with my <a href="" >Udacity Nanodegree</a> and now I do freelance web development and graphic design. </p>
 			</div>
 		)
 	}
