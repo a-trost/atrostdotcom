802022278195595496093afd42be970facd99c71
Alexander Trost <AlexRTrost@gmail.com>
2018-07-13 07:08:45 -0400

    style: Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 import PostListing from "../components/Posts/PostListing";
 
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
+  <React.Fragment>
   <div>
     <h2>Recent Articles</h2>
-  {data.allMarkdownRemark.edges.map(({node}) => ( <PostListing key={node.id} post={node} />
-  )
-  )}
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <PostListing key={node.id} post={node} />
+    ))}
   </div>
-)
+  </React.Fragment>
+);
 
-export default IndexPage
+export default IndexPage;
 
 export const query = graphql`
   query SiteMeta {
@@ -21,22 +23,21 @@ export const query = graphql`
         desc
       }
     }
-  allMarkdownRemark(sort: {
-    fields: [frontmatter___date],
-    order: DESC
-  }) {
-    edges {
-      node {
-        id
-        fields{slug}
-      frontmatter{
-        title
-        date
-      }
-        html
-        excerpt(pruneLength: 280)
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            date
+          }
+          html
+          excerpt(pruneLength: 280)
+        }
       }
     }
   }
-  }
-`
+`;
