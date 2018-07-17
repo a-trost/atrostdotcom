da16afc15e7d01cf431a8225f555bffe81478dbd
Alexander Trost <AlexRTrost@gmail.com>
2018-07-17 16:06:09 -0400

    Add styles for header, image, etc on PostPage

--- src/posts/PostPage.js
+++ src/posts/PostPage.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const BlogHeader = styled.h1`
+  margin:40px 20px;
+  padding:0;
+  color: #333;
+  border:0;
+  font-size: 3rem;
+  line-height: 1
+  text-rendering: optimizeLegibility;
+  text-align:center
+`;
+
+const BlogCoverImage = styled.img`
+  border-radius: 10px;
+`;
+
+const BlogDate = styled.p`
+  line-height: 2rem;
+  color: #aaa;
+  font-size: 0.9rem;
+  text-transform: uppercase;
+`;
 
 export default class PostPage extends Component {
   render() {
-    const {data} = this.props;
+    const { data } = this.props;
     if (!data) return null;
-    return <div>
-			        <span>{data.markdownRemark.frontmatter.date}</span>
-			<h1>{data.markdownRemark.frontmatter.title}</h1>
-			<div dangerouslySetInnerHTML={{__html:data.markdownRemark.html}} />
-		</div>;
+    return (
+      <div>
+        <BlogHeader>{data.markdownRemark.frontmatter.title}</BlogHeader>
+
+        <BlogCoverImage src={data.markdownRemark.frontmatter.image} />
+        <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        <BlogDate>{data.markdownRemark.frontmatter.date}</BlogDate>
+      </div>
+    );
   }
 }
 
@@ -19,6 +46,7 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM DD YYYY")
+        image
       }
     }
   }
