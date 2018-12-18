c42c49dbff09ad2741b4843e4a86873c9e264e42
Alexander Trost <AlexRTrost@gmail.com>
2018-12-18 06:52:20 -0500

    Add fallback for browsers without JS

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+const React = require("react");
 
- // You can delete this file if you're not using it
+exports.onRenderBody = ({ setPreBodyComponents }) => {
+  setPreBodyComponents([
+    <noscript key="noscript">
+      Your browser does not support JavaScript, which this site needs to load
+      properly!
+    </noscript>,
+  ]);
+};
