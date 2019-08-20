baa180cea3152ca2092a10133d6d0d8366263210
Alex Trost <atrost@greencheckverified.com>
2019-08-20 15:53:50 -0400

    Increase scale ratio on headers

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -9,6 +9,7 @@ noriegaTheme.headerWeight = "300";
 noriegaTheme.headerGray = "80";
 noriegaTheme.headerGrayHue = "200";
 noriegaTheme.baseFontSize = "18px";
+noriegaTheme.scaleRatio = 3;
 noriegaTheme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
   "h1,h2,h3,h4,h5,h6": {
     marginBottom: rhythm(1 / 2),
