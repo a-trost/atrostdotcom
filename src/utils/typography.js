a2b05a4103e90f67aedd7e0f422c23de807a6313
Alex Trost <atrost@greencheckverified.com>
2019-08-20 09:20:10 -0400

    Update typography.js styles

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,35 +1,27 @@
 import Typography from "typography";
 import noriegaTheme from "typography-theme-noriega";
 
-noriegaTheme.googleFonts = [
-  {
-    name: "Raleway",
-    styles: ["300", "700"],
+noriegaTheme.headerFontFamily = ["Raleway", "Helvetica Neue", "sans-serif"];
+
+noriegaTheme.bodyFontFamily = ["Open Sans", "Helvetica Neue", "sans-serif"];
+
+noriegaTheme.headerWeight = "300";
+noriegaTheme.headerGray = "80";
+noriegaTheme.headerGrayHue = "200";
+noriegaTheme.baseFontSize = "18px";
+noriegaTheme.overrideThemeStyles = ({ rhythm }, options, styles) => ({
+  "h1,h2,h3,h4,h5,h6": {
+    marginBottom: rhythm(1 / 2),
+    marginTop: rhythm(2),
+    color: "hsl(219, 41%, 30%);",
   },
-  {
-    name: "Open Sans",
-    styles: ["400", "400i", "700", "700i"],
-  },
-];
-
-noriegaTheme.headerFontFamily = [
-  "Raleway",
-  "Helvetica Neue",
-  "Segoe UI",
-  "Helvetica",
-  "Arial",
-  "sans-serif",
-];
-
-noriegaTheme.bodyFontFamily = [
-  "Open Sans",
-  "Helvetica Neue",
-  "Segoe UI",
-  "Helvetica",
-  "Arial",
-  "sans-serif",
-];
-
+});
 const typography = new Typography(noriegaTheme);
 
+if (process.env.NODE_ENV !== "production") {
+  typography.injectStyles();
+}
+
 export default typography;
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
