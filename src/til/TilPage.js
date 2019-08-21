8c417177a5f1e445da3073d33665dcdc016f4332
Alex Trost <atrost@greencheckverified.com>
2019-08-21 09:10:18 -0400

    Remove unused components

--- src/til/TilPage.js
+++ src/til/TilPage.js
@@ -1,64 +1,7 @@
 import React, { Component } from "react";
 import { graphql } from "gatsby";
-import styled from "styled-components";
 import Layout from "../components/Layout";
 import TILCard from "../components/TILCard";
-
-const TilHeader = styled.h1`
-  margin: 5px;
-  padding:0;
-  color: #444;
-  border:0;
-  font-size: 2rem;
-  line-height: 1
-  text-rendering: optimizeLegibility;
-  text-align:center;
-  @media all and (max-width:500px){
-    font-size: 2rem;
-  }
-`;
-const TilDate = styled.p`
-  line-height: 0.9rem;
-  color: #aaa;
-  font-size: 0.9rem;
-  text-align: center;
-  max-width: unset;
-  margin-bottom: 2rem;
-`;
-
-const TilWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-`;
-
-const TilContent = styled.div`
-  display: flex;
-  width: 100%;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
-  p,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    max-width: 680px;
-    width: 100%;
-  }
-  .gatsby-resp-image-link {
-    box-shadow: 5px 5px 20px 0 rgba(46, 61, 73, 0.15);
-  }
-  .gatsby-highlight {
-    align-self: flex-start;
-    justify-self: flex-start;
-    max-width: 100%;
-  }
-`;
 
 export default class PostPage extends Component {
   render() {
