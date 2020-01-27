81f8dd42facb33404e87077360fa286bc70f0f83
Alexander Trost <alexrtrost@gmail.com>
2020-01-26 21:26:49 -0500

    Bring back twitter and Facebook cards

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -171,25 +171,25 @@ export default ({ postNode, postPath, postSEO, aboutSEO }) => {
       <script type="application/ld+json">
         {JSON.stringify(schemaOrgJSONLD)}
       </script>
-      {/* 
-        <meta property="og:url" content={postSEO ? postURL : blogURL} />
-        {postSEO ? <meta property="og:type" content="article" /> : null}
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
-        <meta
-          property="fb:app_id"
-          content={config.siteFBAppID ? config.siteFBAppID : ""}
-        />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:creator"
-          content={config.userTwitter ? config.userTwitter : ""}
-        />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} /> */}
+      <meta property="og:url" content={postSEO ? postURL : blogURL} />
+      {postSEO ? <meta property="og:type" content="article" /> : null}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta
+        property="fb:app_id"
+        content={config.siteFBAppID ? config.siteFBAppID : ""}
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:creator"
+        content={config.userTwitter ? config.userTwitter : ""}
+      />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
 
       <meta name="title" content={config.siteTitle} />
       <meta name="description" content={config.siteDescription} />
